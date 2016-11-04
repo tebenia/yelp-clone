0bcae9b67341199717d005f6358b5f2efa5b50f4
tebenia <tebe.deye@gmail.com>
2016-11-05 06:53:08 +0700

    6. Adding Aliases. So Instead of Referencing to Relative Path We Can Call The Aliases

--- webpack.config.js
+++ webpack.config.js
@@ -79,4 +79,12 @@ config.plugins = [
   new webpack.DefinePlugin(defines)
 ].concat(config.plugins);
 
+config.resolve.root = [src, modules]
+config.resolve.alias = {
+  'css': join(src, 'styles'),
+  'containers': join(src, 'containers'),
+  'components': join(src, 'components'),
+  'utils': join(src, 'utils')
+}
+
 module.exports = config;
