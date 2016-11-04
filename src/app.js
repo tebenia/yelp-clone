1c2a609adcdde024893b3e65a35a70e42e2db264
tebenia <tebe.deye@gmail.com>
2016-11-05 00:23:24 +0700

    2. App ready to build
    App.js as a starter

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = React.createClass({
+  render () {
+    return (
+      <div>Hello World!</div>
+    )
+  }
+})
+
+const mountNode = document.querySelector("#root");
+ReactDOM.render(<App/>, mountNode);
